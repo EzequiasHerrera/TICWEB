433ff667360b60bad9c0267ee122c1a718a8b6af
Ezequías Herrera <58568497+EzequiasHerrera@users.noreply.github.com>
2025-04-17 16:40:15 -0300

    Images fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'url';
 
-export default defineConfig(({ mode }) => {
-  // Cargar las variables de entorno
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    base: '/',
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_BACKEND_URL,
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: false,
       },
     },
-  };
+  },
+  base: '/',
 });
